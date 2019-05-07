494f39586bea22055244134beca622e5ceea01fe
Lee Fieldhouse <leebtc1991@gmail.com>
2019-05-07 19:33:52 +0100

    add layout and style for index.blade

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,6 +4,203 @@
 @endsection
 @section('content')
 <div class="wrapper">
-Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam tempore alias temporibus totam cupiditate magnam odio aperiam, quos expedita quasi neque ratione animi maiores commodi nostrum ipsam consequatur error pariatur eum ullam ducimus quod voluptatibus. Temporibus non ad placeat voluptates nihil saepe exercitationem dolorum ratione odio, amet, quos accusantium atque adipisci quaerat aliquam debitis eaque et. Ipsam quidem aperiam cum ullam alias accusamus esse expedita! Mollitia vel dolorem sapiente error quos est veniam laborum nesciunt! Officiis vitae, blanditiis odio harum expedita accusantium numquam asperiores, rem id dolorum libero! Fugiat, modi? Accusantium ut fugiat repudiandae ab ullam veritatis obcaecati nisi asperiores odio suscipit nihil saepe rem porro debitis accusamus iure quo dolorem cumque, molestiae officiis vel? Eos reprehenderit est nemo! Asperiores, laudantium. Itaque enim officia obcaecati beatae accusantium, reiciendis vel consequuntur sunt sequi cumque corrupti commodi debitis eaque, ipsa praesentium labore et deleniti reprehenderit aperiam tempora ipsam eius ullam molestias quos? Libero exercitationem debitis quaerat quis repellat quibusdam illum, excepturi porro ipsum assumenda cumque optio quia dolore commodi dolorem nesciunt dolores provident sint voluptatum consectetur labore alias mollitia? Dicta error quas doloremque perspiciatis! Deleniti sint dolore quam perspiciatis, quia aliquid natus impedit quaerat dolorum commodi sequi odit provident facere quidem beatae.
+    <div class="section-title">
+        Recommended
+    </div>
+    <div class="video-wrapper">
+        {{-- start video card --}}
+        <div class="video-preview-card">
+            <img src="https://ipiccy.com/blog/wp-content/uploads/2016/12/Youtube-thumbnails.jpg" alt="" class="video-preview-card-video">
+            <div class="video-preview-card-content">
+                <div class="video-preview-card-title">
+                    Mumbai Mall Party
+                </div>
+                <div class="video-preview-card-channel">
+                    bald and bankrupt
+                </div>
+                <div class="video-preview-card-info">
+                    3M views - 2 years ago
+                </div>
+            </div>
+        </div>
+        {{-- end video card --}}
+        {{-- start video card --}}
+        <div class="video-preview-card">
+            <img src="https://ipiccy.com/blog/wp-content/uploads/2016/12/Youtube-thumbnails.jpg" alt="" class="video-preview-card-video">
+            <div class="video-preview-card-content">
+                <div class="video-preview-card-title">
+                    Mumbai Mall Party
+                </div>
+                <div class="video-preview-card-channel">
+                    bald and bankrupt
+                </div>
+                <div class="video-preview-card-info">
+                    3M views - 2 years ago
+                </div>
+            </div>
+        </div>
+        {{-- end video card --}}
+        {{-- start video card --}}
+        <div class="video-preview-card">
+            <img src="https://ipiccy.com/blog/wp-content/uploads/2016/12/Youtube-thumbnails.jpg" alt="" class="video-preview-card-video">
+            <div class="video-preview-card-content">
+                <div class="video-preview-card-title">
+                    Mumbai Mall Party
+                </div>
+                <div class="video-preview-card-channel">
+                    bald and bankrupt
+                </div>
+                <div class="video-preview-card-info">
+                    3M views - 2 years ago
+                </div>
+            </div>
+        </div>
+        {{-- end video card --}}
+        {{-- start video card --}}
+        <div class="video-preview-card">
+            <img src="https://ipiccy.com/blog/wp-content/uploads/2016/12/Youtube-thumbnails.jpg" alt="" class="video-preview-card-video">
+            <div class="video-preview-card-content">
+                <div class="video-preview-card-title">
+                    Mumbai Mall Party
+                </div>
+                <div class="video-preview-card-channel">
+                    bald and bankrupt
+                </div>
+                <div class="video-preview-card-info">
+                    3M views - 2 years ago
+                </div>
+            </div>
+        </div>
+        {{-- end video card --}}
+        {{-- start video card --}}
+        <div class="video-preview-card">
+            <img src="https://ipiccy.com/blog/wp-content/uploads/2016/12/Youtube-thumbnails.jpg" alt="" class="video-preview-card-video">
+            <div class="video-preview-card-content">
+                <div class="video-preview-card-title">
+                    Mumbai Mall Party
+                </div>
+                <div class="video-preview-card-channel">
+                    bald and bankrupt
+                </div>
+                <div class="video-preview-card-info">
+                    3M views - 2 years ago
+                </div>
+            </div>
+        </div>
+        {{-- end video card --}}
+        {{-- start video card --}}
+        <div class="video-preview-card">
+            <img src="https://ipiccy.com/blog/wp-content/uploads/2016/12/Youtube-thumbnails.jpg" alt="" class="video-preview-card-video">
+            <div class="video-preview-card-content">
+                <div class="video-preview-card-title">
+                    Mumbai Mall Party
+                </div>
+                <div class="video-preview-card-channel">
+                    bald and bankrupt
+                </div>
+                <div class="video-preview-card-info">
+                    3M views - 2 years ago
+                </div>
+            </div>
+        </div>
+        {{-- end video card --}}
+        {{-- start video card --}}
+        <div class="video-preview-card">
+            <img src="https://ipiccy.com/blog/wp-content/uploads/2016/12/Youtube-thumbnails.jpg" alt="" class="video-preview-card-video">
+            <div class="video-preview-card-content">
+                <div class="video-preview-card-title">
+                    Mumbai Mall Party
+                </div>
+                <div class="video-preview-card-channel">
+                    bald and bankrupt
+                </div>
+                <div class="video-preview-card-info">
+                    3M views - 2 years ago
+                </div>
+            </div>
+        </div>
+        {{-- end video card --}}
+        {{-- start video card --}}
+        <div class="video-preview-card">
+            <img src="https://ipiccy.com/blog/wp-content/uploads/2016/12/Youtube-thumbnails.jpg" alt="" class="video-preview-card-video">
+            <div class="video-preview-card-content">
+                <div class="video-preview-card-title">
+                    Mumbai Mall Party
+                </div>
+                <div class="video-preview-card-channel">
+                    bald and bankrupt
+                </div>
+                <div class="video-preview-card-info">
+                    3M views - 2 years ago
+                </div>
+            </div>
+        </div>
+        {{-- end video card --}}
+        {{-- start video card --}}
+        <div class="video-preview-card">
+            <img src="https://ipiccy.com/blog/wp-content/uploads/2016/12/Youtube-thumbnails.jpg" alt="" class="video-preview-card-video">
+            <div class="video-preview-card-content">
+                <div class="video-preview-card-title">
+                    Mumbai Mall Party
+                </div>
+                <div class="video-preview-card-channel">
+                    bald and bankrupt
+                </div>
+                <div class="video-preview-card-info">
+                    3M views - 2 years ago
+                </div>
+            </div>
+        </div>
+        {{-- end video card --}}
+        {{-- start video card --}}
+        <div class="video-preview-card">
+            <img src="https://ipiccy.com/blog/wp-content/uploads/2016/12/Youtube-thumbnails.jpg" alt="" class="video-preview-card-video">
+            <div class="video-preview-card-content">
+                <div class="video-preview-card-title">
+                    Mumbai Mall Party
+                </div>
+                <div class="video-preview-card-channel">
+                    bald and bankrupt
+                </div>
+                <div class="video-preview-card-info">
+                    3M views - 2 years ago
+                </div>
+            </div>
+        </div>
+        {{-- end video card --}}
+        {{-- start video card --}}
+        <div class="video-preview-card">
+            <img src="https://ipiccy.com/blog/wp-content/uploads/2016/12/Youtube-thumbnails.jpg" alt="" class="video-preview-card-video">
+            <div class="video-preview-card-content">
+                <div class="video-preview-card-title">
+                    Mumbai Mall Party
+                </div>
+                <div class="video-preview-card-channel">
+                    bald and bankrupt
+                </div>
+                <div class="video-preview-card-info">
+                    3M views - 2 years ago
+                </div>
+            </div>
+        </div>
+        {{-- end video card --}}
+        {{-- start video card --}}
+        <div class="video-preview-card">
+            <img src="https://ipiccy.com/blog/wp-content/uploads/2016/12/Youtube-thumbnails.jpg" alt="" class="video-preview-card-video">
+            <div class="video-preview-card-content">
+                <div class="video-preview-card-title">
+                    Mumbai Mall Party
+                </div>
+                <div class="video-preview-card-channel">
+                    bald and bankrupt
+                </div>
+                <div class="video-preview-card-info">
+                    3M views - 2 years ago
+                </div>
+            </div>
+        </div>
+        {{-- end video card --}}
+
+    </div>
 </div>
 @endsection
