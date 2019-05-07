d72d4cc8b680add465ebccaf93fe8368b0f7db74
Lee Fieldhouse <leebtc1991@gmail.com>
2019-05-07 22:45:54 +0100

    sorted video size

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -17,11 +17,14 @@
     </head>
     <body>
         @include('layouts.navbar')
+        <div class="main-section">
+            </div>
         @include('layouts.sidebar')
         @auth
             @include('layouts.uploadform')
         @endauth
         @yield('content')
+
         <script
   src="https://code.jquery.com/jquery-3.4.0.js"
   integrity="sha256-DYZMCC8HTC+QDr5QNaIcfR7VSPtcISykd+6eSmBW5qo="
--- resources/views/video/show.blade.php
+++ resources/views/video/show.blade.php
@@ -3,5 +3,31 @@
     {{$video->title}}
 @endsection
 @section('content')
-    <video controls autoplay src="{{asset($video->video_url)}}"></video>
+<div class="main-video">
+<video
+
+    controls
+    autoplay
+>
+    <source
+        src="{{asset($video->video_url)}}"
+        type="video/{{$video->file_type}}"
+    />
+    Your browser does not support the video tag.
+</video>
+</div>
+    {{-- <div class="wrapper">
+        <div class="video-page-wrapper">
+        <div class="video-page-main-col">
+            <div class="section-title">
+                {{$video->title}}
+            </div>
+        </div>
+        <div class="video-page-side-col">
+            <div class="section-title">
+                {{$video->title}}
+            </div>
+        </div>
+        </div>
+    </div> --}}
 @endsection
