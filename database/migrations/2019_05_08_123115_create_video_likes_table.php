c92b090b72a7da620c7378b2666e3bdc29441bb1
Lee Fieldhouse <leebtc1991@gmail.com>
2019-05-08 13:32:56 +0100

    Created VideoLike -mc

---
+++ database/migrations/2019_05_08_123115_create_video_likes_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVideoLikesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('video_likes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('video_likes');
+    }
+}
