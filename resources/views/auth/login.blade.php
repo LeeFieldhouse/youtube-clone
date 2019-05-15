d08d856137cb2bbcf19773b1439539f260679986
Lee Fieldhouse <leebtc1991@gmail.com>
2019-05-15 09:39:04 +0100

    update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,17 +4,27 @@
 <div class="wrapper">
         <div class="form-wrapper">
             <form class="form-card" action="{{route('login')}}" method="POST">
+                @csrf
                 <div class="form-card-heading">
                     Login
                 </div>
                 <div class="form-card-content">
                     <div class="form-input-section">
                         <div class="form-input-label">
-                            Name
+                            Email
                         </div>
+                        <input class="form-input-input" name="email" type="text">
                     </div>
-
+                    <br>
+                    <div class="form-input-section">
+                        <div class="form-input-label">
+                            Password
+                        </div>
+                        <input class="form-input-input" name="password" type="password">
+                    </div>
+                    <br>
                 </div>
+                <button type="submit" class="form-btn">Sign In</button>
             </form>
         </div>
     </div>
--- resources/views/layouts/uploadform.blade.php
+++ resources/views/layouts/uploadform.blade.php
@@ -4,6 +4,6 @@
         <input type="file" name="video_video" id="">
         <input type="text" name="video_title" placeholder="Title">
         <textarea name="video_description" id="" ></textarea>
-        <input type="hidden" name="video-user" value="{{Auth::user()->username}}">
+
     </form>
 </div>
